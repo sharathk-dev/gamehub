e4e5dfd08b7617d042b635d97b7df224a480859c
sharathkuchal <sharathkuchal@gmail.com>
2025-09-27 20:04:54 +0530

    updated button to inline button

--- src/components/ExpandableText.tsx
+++ src/components/ExpandableText.tsx
@@ -5,8 +5,10 @@ interface Props {
   children: string;
 }
 const ExpandableText = ({ children }: Props) => {
-  const LIMIT = 300;
+  const LIMIT = 700;
   const [expanded, setExpanded] = useState(false);
+
+  if (!children) return null;
 
   if (children.length < LIMIT) return <Text>{children}</Text>;
 
@@ -14,17 +16,20 @@ const ExpandableText = ({ children }: Props) => {
 
   return (
     <>
-      <Text>{expanded ? children : summary}</Text>
-      <Button
-        colorPalette="yellow"
-        rounded="lg"
-        size="xs"
-        fontWeight="bold"
-        variant="solid"
-        onClick={() => setExpanded(!expanded)}
-      >
-        {expanded ? 'Show Less' : 'Show More'}
-      </Button>
+      <Text>
+        {expanded ? children : summary}
+        <Button
+          colorPalette="yellow"
+          rounded="lg"
+          size="xs"
+          marginLeft={1}
+          fontWeight="bold"
+          variant="solid"
+          onClick={() => setExpanded(!expanded)}
+        >
+          {expanded ? 'Show Less' : 'Show More'}
+        </Button>
+      </Text>
     </>
   );
 };
