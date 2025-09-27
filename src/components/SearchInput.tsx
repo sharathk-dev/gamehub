8135eb0c51e35105677ab7ac1c874b0b793ab156
sharathkuchal <sharathkuchal@gmail.com>
2025-09-27 23:32:55 +0530

    fix nav bar

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,16 +2,19 @@ import useGameQueryStore from '@/store';
 import { Input, InputGroup } from '@chakra-ui/react';
 import { useRef } from 'react';
 import { LuSearch } from 'react-icons/lu';
+import { useNavigate } from 'react-router-dom';
 
 const SearchInput = () => {
   const ref = useRef<HTMLInputElement>(null);
   const setSearchText = useGameQueryStore(s => s.setSearchText);
+  const navigate = useNavigate();
 
   return (
     <form
       onSubmit={event => {
         event.preventDefault();
         if (ref.current) setSearchText(ref.current.value);
+        navigate('/');
       }}
     >
       <InputGroup startElement={<LuSearch />}>
